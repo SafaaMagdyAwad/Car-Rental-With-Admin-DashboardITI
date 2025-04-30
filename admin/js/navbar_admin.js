f538e1be104e914a45c320d11e6f5aa5a8858388
emanibrahim <emanibrahimibrahimabdelgawwad@gmail.com>
2025-04-30 16:46:52 +0200

    dashboard

--- admin/js/navbar_admin.js
+++ admin/js/navbar_admin.js
@@ -1,34 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const offCanvas = document.querySelector('.offcanvas');
-    const toggleButton = document.getElementById('toggleButton');
-    const row_parent = document.getElementById('row_parent');
-    const offcanvas_div = document.getElementById('offcanvas_div');
+// document.addEventListener("DOMContentLoaded", () => {
+//     const offCanvas = document.querySelector('.offcanvas');
+//     const toggleButton = document.getElementById('toggleButton');
+//     const row_parent = document.getElementById('row_parent');
+//     // const offcanvas_div = document.getElementById('offcanvas_div');
     
-    // 
+//     // 
     
-    const mediaQuery = window.matchMedia('(max-width: 768px)');
+//     const mediaQuery = window.matchMedia('(max-width: 768px)');
     
-    function handleScreenChange(e) {
-      if (e.matches) {
-        offCanvas.classList.remove('show');
-        offCanvas.classList.remove('offcanvas-start');
-        offCanvas.classList.add('offcanvas-top');
-        toggleButton.classList.remove('d-none');
-        offcanvas_div.remove()
-        // offcanvas-top
+//     function handleScreenChange(e) {
+//       if (e.matches) {
+//         offCanvas.classList.remove('show');
+//         offCanvas.classList.remove('offcanvas-start');
+//         offCanvas.classList.add('offcanvas-top');
+//         toggleButton.classList.remove('d-none');
+//         toggleButton.classList.add('d-block')
+//         offcanvas.remove()
+//         // offcanvas-top
     
-      } else {
-        // Screen is wider than 768px
-        row_parent.appendChild(offcanvas_div);
-        offCanvas.classList.add('show'); 
-        offCanvas.classList.remove('offcanvas-top');
-        offCanvas.classList.add('offcanvas-start');
-        toggleButton.classList.add('d-none'); 
+//       } else {
+//         // Screen is wider than 768px
+//         row_parent.appendChild(offcanvas);
+//         offCanvas.classList.add('show'); 
+//         offCanvas.classList.remove('offcanvas-top');
+//         offCanvas.classList.add('offcanvas-start');
+//         toggleButton.classList.add('d-none'); 
     
-      }
+//       }
     
-    }
+//     }
     
-    mediaQuery.addEventListener('change', handleScreenChange);
+//     mediaQuery.addEventListener('change', handleScreenChange);
     
-    });
+//     });
