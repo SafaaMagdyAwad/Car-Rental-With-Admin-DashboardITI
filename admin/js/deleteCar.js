5312ef397636f52cd2826cd9fe2a7c84d82c05b0
eslamlaiali <islamlaiali@gmail.com>
2025-05-02 03:12:23 +0300

    validate delete -if the car is booked for a late date the car wont be deleted-

--- admin/js/deleteCar.js
+++ admin/js/deleteCar.js
@@ -1,12 +1,24 @@
 export function deleteCar(id) {
     let confirmVal = confirm("are you sure to delete this car?");
+    let requestedCars = JSON.parse(localStorage.getItem("bookedCars"));
+    let currentdate = new Date();
+    let confirmedCars = requestedCars.filter((el)=>{
+        let elDate = new Date(el['drop-date'] + ":" + el['drop-time']);
+        return (el.status == "confirmed" && elDate > currentdate);
+    });
+    let confirmedCarsIds = [];
+    confirmedCars.forEach(element => {
+        confirmedCarsIds.push(element['car-id']);
+    });
     if (confirmVal == true) {
         let rawCars = localStorage.getItem("cars");
         let cars = JSON.parse(rawCars);
         for (let index = 0; index < cars.length; index++) {
             
-            if(cars[index].id == id){
+            if(cars[index].id == id && !(confirmedCarsIds.includes(id))){
                 cars.splice(index , 1);
+            }else if(cars[index].id == id && confirmedCarsIds.includes(id)){
+                alert("this car can not be deleted");
             }
             
         }
